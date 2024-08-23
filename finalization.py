c90ddb1e8009ea3c1d5939ec9e4c7ade8d479bf2
Šimon Jech <smnjech2@gmail.com>
2024-08-23 16:20:11 +0200

    oprava

--- finalization.py
+++ finalization.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from initialstate import pociatocny_stav, energia, magnetizacia
-from metropolis import relaxacia, zmena_stavu
+from metropolis import relaxacia, zmena_stavu, vyvoj_stavu
 import matplotlib.animation as animation
 
 #funkcia na výpočet strednej hodnoty energie 
@@ -21,13 +21,6 @@ def stredovani (m,T,r,n): #m je počet stredovaných energií, T je teplota, r j
 
     return H / m, M / m
 
-# def stredna_magnetizacia (k,T,r,n):
-#     M=0
-#     for i in range (k):
-#         M+=magnetizacia(relaxacia(pociatocny_stav(n),r,T)[i])/k
-#     return M
-
-#vytvorí súbor energií v rozsahu teplôt T1 až T2.
 
 def stredovani_v_rozsahu (teploty,m,r,n):
     energie=[]
@@ -38,15 +31,6 @@ def stredovani_v_rozsahu (teploty,m,r,n):
         magnetizace.append(M)
     return energie,magnetizace
 
-#vytvorí súbor magnetizácií v rozsahu teplôt T1 až T2.
-
-# def magnetizacie_v_rozsahu (T1,T2,k,r,n):
-#     magnetizacie=[]
-#     teploty=[]
-#     for T in range (T1,T2):
-#         magnetizacie.append(stredna_magnetizacia(k,T,r,n))
-#         teploty.append(T)
-#     return [teploty,magnetizacie]
 
 m = 200
 r = 200
@@ -74,25 +58,5 @@ ax2.set_title('Závislosť magnetizácie od teploty')
 
 plt.tight_layout()
 plt.show()
-#zobrazenie vývinu stavu 
 
-n=20 #rozmer mriežky 
-T=1 # teplota
-r=10 # počet relaxovaných stavov
-A=relaxacia(pociatocny_stav(n),r,T)
-
-# Vytvorenie animácie
-fig, ax = plt.subplots()
-
-# Inicializácia mriežky
-im = ax.imshow(A[0], cmap='gray')
-ax.set_title(f'Vývoj stavu - Teplota $T={T}$, Počet stavov $r={r}$.')
-
-def update(frame):
-    im.set_array(A[frame])
-    return [im]
-
-ani = animation.FuncAnimation(fig, update, frames=len(A), interval=400, blit=True)
-
-plt.show()
 
